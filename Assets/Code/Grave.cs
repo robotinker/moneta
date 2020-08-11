ace548a59d29882a7eb178f375ce5541f6067723
Ted Carter <onewhocarts@gmail.com>
2020-08-11 16:10:26 -0700

    Adding art, creating gravestones from folders in graveyard.

---
+++ Assets/Code/Grave.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Grave : MonoBehaviour
+{
+    public TextMeshPro TitleText;
+    public TextMeshPro DateText;
+    public TextMeshPro DescriptionText;
+
+    public void Init(string title, DateTime date, string description)
+    {
+        TitleText.text = title;
+        DescriptionText.text = description;
+        DateText.text = string.Format("({0})", date.ToString("MMM, yyyy"));
+    }
+}
--- Assets/Code/GraveyardLoader.cs
+++ Assets/Code/GraveyardLoader.cs
@@ -2,12 +2,16 @@
 using System;
 using System.IO;
 using UnityEngine;
+using UnityRandom = UnityEngine.Random;
 
 public class GraveyardLoader : MonoBehaviour
 {
     public GameObject GravePrefab;
+    public Transform GraveParent;
 
     public static GraveyardLoader Instance;
+
+    const string GraveyardPathKey = "GraveyardPath";
 
     private void Awake()
     {
@@ -17,6 +21,11 @@ public class GraveyardLoader : MonoBehaviour
     private void Start()
     {
         ConfirmationDialog.Instance.Hide();
+
+        if (PlayerPrefs.HasKey(GraveyardPathKey))
+        {
+            Setup(PlayerPrefs.GetString(GraveyardPathKey));
+        }
     }
 
     public void StartInteraction()
@@ -33,8 +42,12 @@ public class GraveyardLoader : MonoBehaviour
         if (paths.Length > 0)
         {
             var directoryPath = paths[0];
-            var directories = Directory.EnumerateDirectories(directoryPath);
-            Debug.Log("Directories: " + string.Join(", ", directories));
+            if (!PlayerPrefs.HasKey(GraveyardPathKey) || directoryPath != PlayerPrefs.GetString(GraveyardPathKey))
+            {
+                PlayerPrefs.SetString(GraveyardPathKey, directoryPath);
+                Teardown();
+                Setup(directoryPath);
+            }
         }
         EndInteraction();
     }
@@ -44,5 +57,26 @@ public class GraveyardLoader : MonoBehaviour
         ConfirmationDialog.Instance.Hide();
         Cursor.lockState = CursorLockMode.Locked;
         GameState.Instance.CurrentState = GameState.State.World;
+    }
+
+    void Setup(string directory)
+    {
+        var directories = Directory.EnumerateDirectories(directory);
+        Debug.Log("Directories: " + string.Join(", ", directories));
+
+        foreach (var path in directories)
+        {
+            var newGrave = Instantiate(GravePrefab, GraveParent);
+            newGrave.transform.position = Vector3.zero + Vector3.right * UnityRandom.Range(-5f, 5f) + Vector3.forward * UnityRandom.Range(-5f, 5f);
+            newGrave.GetComponent<Grave>().Init(Path.GetFileName(path), Directory.GetCreationTime(path), "Lorem ipsum si cut dolor meus.");
+        }
+    }
+
+    void Teardown()
+    {
+        for (var i = 0; i < GraveParent.childCount; i++)
+        {
+            Destroy(GraveParent.GetChild(i));
+        }
     }
 }
