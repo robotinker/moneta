aafd2a1121f5cd18889044c4422541c26a027f51
Ted Carter <onewhocarts@gmail.com>
2020-08-16 01:57:41 -0700

    Adding particle effects to burial animation

---
+++ Assets/Code/PrefabInstantiator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabInstantiator : MonoBehaviour
+{
+    public Transform Parent;
+
+    public void CreatePrefab(GameObject prefab)
+    {
+        Instantiate(prefab, Parent);
+    }
+}
